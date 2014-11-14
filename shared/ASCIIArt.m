d53104bbc92f9b0f8151b8515bf6f07e28008fc3
sonson <yoshida.yuichi@gmail.com>
2014-11-14 17:21:55 +0900

    CoreDataのモデル設計，クラスを追加．

---
+++ shared/ASCIIArt.m
@@ -0,0 +1,20 @@
+//
+//  ASCIIArt.m
+//  AAKeyboardApp
+//
+//  Created by sonson on 2014/11/14.
+//  Copyright (c) 2014年 sonson. All rights reserved.
+//
+
+#import "ASCIIArt.h"
+#import "ASCIIArtGroup.h"
+
+
+@implementation ASCIIArt
+
+@dynamic text;
+@dynamic ratio;
+@dynamic lastUsedTime;
+@dynamic relationship;
+
+@end
---
+++ shared/ASCIIArtGroup.m
@@ -0,0 +1,18 @@
+//
+//  ASCIIArtGroup.m
+//  AAKeyboardApp
+//
+//  Created by sonson on 2014/11/14.
+//  Copyright (c) 2014年 sonson. All rights reserved.
+//
+
+#import "ASCIIArtGroup.h"
+
+
+@implementation ASCIIArtGroup
+
+@dynamic title;
+@dynamic order;
+@dynamic type;
+
+@end
