890670bdd113c5e4adcbefe299295befc155b958
sonson <yoshida.yuichi@gmail.com>
2014-11-25 08:38:40 +0900

    グループリネーム用のビューコントローラを追加．

---
+++ shared/AAKGroupRenameViewController.h
@@ -0,0 +1,15 @@
+//
+//  AAKGroupRenameViewController.h
+//  AAKeyboardApp
+//
+//  Created by sonson on 2014/11/25.
+//  Copyright (c) 2014年 sonson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAKGroupRenameViewController : UITableViewController
+
+@property (nonatomic, strong) AAKASCIIArtGroup *group;
+
+@end
