1e1ecba67cf0cd24aad66cbbdbf799e75469888c
sonson <yoshida.yuichi@gmail.com>
2015-01-09 01:53:37 +0900

    ヘルプの画像リソースをアップデート．

---
+++ AAKeyboardApp/AAKHelpDummyTapView.h
@@ -0,0 +1,13 @@
+//
+//  AAKHelpDummyTapView.h
+//  AAKeyboardApp
+//
+//  Created by sonson on 2015/01/09.
+//  Copyright (c) 2015年 sonson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAKHelpDummyTapView : UIView
+
+@end
---
+++ AAKeyboardApp/AAKHelpDummyTapView.m
@@ -0,0 +1,21 @@
+//
+//  AAKHelpDummyTapView.m
+//  AAKeyboardApp
+//
+//  Created by sonson on 2015/01/09.
+//  Copyright (c) 2015年 sonson. All rights reserved.
+//
+
+#import "AAKHelpDummyTapView.h"
+
+@implementation AAKHelpDummyTapView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
